d138b39d9d501a9ac41ff13d7a6b85d064a7d68d
Abdullateef alruwaished <abdullateefalruwaished@Abdullateefs-MacBook-Pro.local>
2017-12-11 18:36:03 +0300

    checkout

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,50 @@
 from django.shortcuts import render
 from .models import Cart, CartItem
 from mycoffee.models import Coffee
+from django.shortcuts import redirect
+from .models import Order, UserAddress
+from .forms import AddressForm, AddressSelectForm
+
+def checkout(request):
+    cart, created = Cart.objects.get_or_create(user=request.user)
+    order, created = Order.objects.get_or_create(cart=cart, user=request.user)
+
+    if order.address == None:
+        return redirect("cart:select_address")
+    return redirect("/") #Some payment page
+
+def select_address(request):
+    if UserAddress.objects.filter(user=request.user).count()<1:
+        return redirect("cart:create_address")
+    form = AddressSelectForm()
+    form.fields['address'].queryset = UserAddress.objects.filter(user=request.user)
+    if request.method == 'POST':
+        form = AddressSelectForm(request.POST)
+        if form.is_valid():
+            address = form.cleaned_data['address']
+            order = Order.objects.get(user=request.user)
+            order.address=address
+            order.save()
+            return redirect("cart:checkout")
+    context = {
+        'form':form
+    }
+    return render(request, 'select_address.html', context)
+
+def create_address(request):
+    form = AddressForm()
+    if request.method == 'POST':
+        form = AddressForm(request.POST)
+        if form.is_valid():
+            address =form.save(commit=False)
+            address.user = request.user
+            address.save()
+            form.save()
+            return redirect("cart:select_address")
+    context = {
+        "form": form
+    }
+    return render(request, 'create_address.html', context)
 
 def cart(request):
     cart, created = Cart.objects.get_or_create(user=request.user)
